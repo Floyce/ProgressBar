6066e53a79caf12712a8661125ad07b25de4a479
Floyce <146327158+Floyce@users.noreply.github.com>
2025-03-24 13:07:22 +0300

    Resolved merge conflicts

--- progress_bar.py
+++ progress_bar.py
@@ -1,6 +1,6 @@
 from tqdm import tqdm
 import time
 
-# Simulate a download
+# Simulate a download to start
 for i in tqdm(range(100), desc="Downloading..."):
     time.sleep(0.05)  # Simulates time delay
